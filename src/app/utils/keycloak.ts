a7b67c7aea00e2729cbe6b96ce90eecdf53f9a77
JoelRamiliarijaona <your.email@example.com>
2024-12-24 11:55:32 +0300

    fix: typage

--- src/app/utils/keycloak.ts
+++ src/app/utils/keycloak.ts
@@ -1,10 +1,10 @@
 import Keycloak from 'keycloak-js';
 
 const keycloakConfig = {
-  url: process.env.NEXT_PUBLIC_KEYCLOAK_URL,
-  realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM,
-  clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID,
-};
+  url: process.env.NEXT_PUBLIC_KEYCLOAK_URL || '',
+  realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM || '',
+  clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID || '',
+} as const;
 
 const keycloak = typeof window !== 'undefined' ? new Keycloak(keycloakConfig) : null;
 
